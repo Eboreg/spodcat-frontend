c80ed4e1d3b2f5628beaa57d112ca23833f2ae9a
Robert Huselius <robert@huseli.us>
2025-05-09 21:20:46 +0200

    updatez

--- app/components/podcast/content/episode-leading-icon.ts
+++ app/components/podcast/content/episode-leading-icon.ts
@@ -8,6 +8,10 @@ export interface PodcastContentEpisodeLeadingIconSignature {
 }
 
 export default class PodcastContentEpisodeLeadingIcon extends Component<PodcastContentEpisodeLeadingIconSignature> {
+    get classes() {
+        return `podcast-content-leading-icon theme-${this.args.episode.seasonTheme}`;
+    }
+
     get numberClass() {
         if (this.args.episode.numberString != undefined && this.args.episode.numberString.length > 3) return "small";
         return "";
