8a243734f3a2bd0068ef24612cbfeb4248f4f221
Robert Huselius <robert@huseli.us>
2025-05-20 22:38:23 +0200

    layout fix 2

--- app/models/episode-song.ts
+++ app/models/episode-song.ts
@@ -19,6 +19,17 @@ export default class EpisodeSongModel extends Model {
         return this.artists.map((a) => a.name).join("/");
     }
 
+    get displayString() {
+        let result = "";
+        const artist = this.artistString;
+
+        if (artist) result += `${artist} – `;
+        result += this.title;
+        if (this.comment) result += ` (${this.comment})`;
+
+        return result;
+    }
+
     get timestampString() {
         return timeString(this["start-time"]);
     }
